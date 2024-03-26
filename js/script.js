d562157df63654f7a51da32761d797ec7f5519c4
iTsGiuse <giuse.amato@icloud.com>
2024-03-26 16:36:48 +0100

    feat: add milestone2 and fix code

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
-/* Descrizione:
-Stampare a schermo un messaggio all’interno di un h1, utilizzando i data. */
-
 const { createApp, ref } = Vue
 
 createApp({
     data() {
         return {
-            messaggio: 'Prima volta con Vue',
+            /* COLORA DI ROSSO IL TITOLO */
             red: "text-danger",
-            immagine: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.sonymusic.it%2Fnews%2Fnitro-lalbum-suicidol-e-disco-doro%2F&psig=AOvVaw0s5NFaAp4npU7Z7EAHRpUY&ust=1711552662066000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKj1mNWckoUDFQAAAAAdAAAAABAE'
+            /* MOSTRA IL MESSAGGIO A SCHERMO */
+            messaggio: 'Prima volta con Vue',
+            /* MOSTRA IMMAGINE A SCHERMO */
+            immagine: 'https://cdn.smehost.net/sonymusicit-itprod/wp-content/uploads/2016/05/Nitro-Suicidol-Post-Mortem-news.jpg'
         }
   }
 }).mount('#app')
-
-/* Bonus:
-Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
